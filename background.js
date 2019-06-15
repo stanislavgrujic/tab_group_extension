a3f89a9b93a5839d2827c0fce64f9528cf089db2
stanislavgrujic <sgrujic@execom.eu>
2019-06-15 07:18:09 +0200

    Add tab link on popup button click to local storage

--- background.js
+++ background.js
@@ -1,14 +1,7 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+    chrome.storage.local.clear();
+    chrome.storage.local.set({"defaultGroup": new Array()}, function() {
+      console.log("Default group created");
     });
-    // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    //     chrome.declarativeContent.onPageChanged.addRules([{
-    //       conditions: [new chrome.declarativeContent.PageStateMatcher({
-    //         pageUrl: {hostEquals: 'developer.chrome.com'},
-    //       })
-    //       ],
-    //           actions: [new chrome.declarativeContent.ShowPageAction()]
-    //     }]);
-    // });
+
 });
